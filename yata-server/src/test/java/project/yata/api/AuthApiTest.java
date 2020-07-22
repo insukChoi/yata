080b98c03d57c451e34ea6d9e44ff6f4a85fafcf
jisu <jisu@skoopmedia.co.kr>
2020-07-22 18:25:40 +0900

    auth 단위테스트 작성중 - 성공만 작성

--- yata-server/src/test/java/project/yata/api/AuthApiTest.java
+++ yata-server/src/test/java/project/yata/api/AuthApiTest.java
@@ -1,7 +1,8 @@
 package project.yata.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+
 @WebMvcTest(AuthController.class)
 @AutoConfigureMockMvc
 public class AuthApiTest {
@@ -41,6 +43,7 @@ public class AuthApiTest {
 //        JoinRequest joinRequest = new JoinRequest("admin@yata.com", "지수","0011");
         JoinRequest joinRequest = JoinRequest.builder().email("admin@yata.com").name("지수").password("0011").build();
 
+        given(authService.join(joinRequest)).willReturn(true);
 
         //when
         final ResultActions actions = mockMvc.perform(post("/api/v2/auth/join")
