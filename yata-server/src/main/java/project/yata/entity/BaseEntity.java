1eab53f3b149a93e669761c004a63700d5495624
najisu <kuu2002@naver.com>
2020-07-26 14:15:50 +0900

    BaseEntity 등록일 수정일 자동할당

--- yata-server/src/main/java/project/yata/entity/BaseEntity.java
+++ yata-server/src/main/java/project/yata/entity/BaseEntity.java
@@ -3,9 +3,12 @@ package project.yata.entity;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import java.time.LocalDateTime;
 
 @Getter
@@ -20,4 +23,16 @@ public abstract class BaseEntity {
     private LocalDateTime updatedAt;
 
     private boolean isDeleted;
+
+    @PrePersist
+    public void prePersis() {
+        if(StringUtils.isEmpty(this.createdAt))
+            this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        if(StringUtils.isEmpty(this.updatedAt))
+            this.updatedAt = LocalDateTime.now();
+    }
 }
