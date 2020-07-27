f3a715d2f7d4afa9c23ebca11932c8a34b16975a
jisu <jisu@skoopmedia.co.kr>
2020-07-27 11:16:34 +0900

    Auditing - EntityListener 변경

--- yata-server/src/main/java/project/yata/entity/BaseEntity.java
+++ yata-server/src/main/java/project/yata/entity/BaseEntity.java
@@ -3,6 +3,7 @@ package project.yata.entity;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityListeners;
@@ -13,7 +14,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
-@EntityListeners({Accommodation.class, Account.class, Attraction.class, Budget.class, Plan.class, Restaurant.class, Transport.class, Travel.class})
+@EntityListeners({AuditingEntityListener.class})
 public abstract class BaseEntity {
 
     @CreatedDate
