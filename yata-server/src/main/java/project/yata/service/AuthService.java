f3957c75b408ff45bcd6c42d5b660346414de406
Aiden Na <kuu2002@naver.com>
2020-07-28 10:14:32 +0900

    feature/yata-19 :: JWT - Refresh Token 발급추가 및 관련설정 수정 (#39)
    * JWT - Refresh Token 발급추가 및 관련설정 수정
    
    * Remember 추상화...
    
    * 토큰 유효시간 application.properties에서 가져옴
    
    Co-authored-by: jisu <jisu@skoopmedia.co.kr>

--- yata-server/src/main/java/project/yata/service/AuthService.java
+++ yata-server/src/main/java/project/yata/service/AuthService.java
@@ -1,11 +1,12 @@
 package project.yata.service;
 
 import project.yata.dto.JoinRequest;
+import project.yata.dto.LoginResponse;
 import project.yata.entity.Account;
 
 public interface AuthService {
     boolean checkDuplicateEmail(String email);
     boolean join(JoinRequest joinRequest);
-    String login(String email, String password);
+    LoginResponse login(String email, String password);
 
 }
