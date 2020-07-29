3ccb86918ecdc47ec78184953aa3280d8d188be4
jisu <jisu@skoopmedia.co.kr>
2020-07-29 18:49:32 +0900

    Service Impl 없이 갑니다.

--- yata-server/src/main/java/project/yata/service/AuthService.java
+++ yata-server/src/main/java/project/yata/service/AuthService.java
@@ -1,12 +1,69 @@
 package project.yata.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import project.yata.common.error.exception.DuplicateEmailException;
+import project.yata.common.error.exception.JoinFailedException;
+import project.yata.common.error.exception.LoginFailedException;
+import project.yata.common.util.jwt.JsonWebTokenProvider;
 import project.yata.dto.JoinRequest;
+import project.yata.dto.JoinResponse;
 import project.yata.dto.LoginResponse;
 import project.yata.entity.Account;
+import project.yata.persistence.AccountRepository;
 
-public interface AuthService {
-    boolean checkDuplicateEmail(String email);
-    boolean join(JoinRequest joinRequest);
-    LoginResponse login(String email, String password);
+@Service
+@RequiredArgsConstructor
+public class AuthService {
 
+    private final PasswordEncoder passwordEncoder;
+    private final AccountRepository accountRepository;
+    private final JsonWebTokenProvider jsonWebTokenProvider;
+
+    public boolean checkDuplicateEmail(String email) {
+        if(!StringUtils.isEmpty(accountRepository.findByEmail(email)))
+            throw new DuplicateEmailException("이미 등록되어 있는 이메일주소입니다.");
+        return true;
+    }
+
+    public JoinResponse join(JoinRequest joinRequest) {
+
+        checkDuplicateEmail(joinRequest.getEmail());
+
+        Account account = Account.builder()
+                .email(joinRequest.getEmail())
+                .name(joinRequest.getName())
+                .password(passwordEncoder.encode(joinRequest.getPassword()))
+                .build();
+
+        Account joinResult = accountRepository.save(account);
+
+        if(StringUtils.isEmpty(joinResult))
+            throw new JoinFailedException("회원가입에 문제가 발생하였습니다.");
+
+        return new JoinResponse().joinComplete(joinResult.getEmail(), joinResult.getName());
+    }
+
+    public LoginResponse login(String email, String password) {
+
+        // Todo ID+PW 검증
+        boolean authentication = checkEmailWithPassword();
+
+        if(authentication)
+            throw new LoginFailedException("사용자 인증에 실패하였습니다.");
+
+        String accessToken = jsonWebTokenProvider.generateToken(email, "access");
+        String refreshToken = jsonWebTokenProvider.generateToken(email, "refresh");
+
+        return new LoginResponse().generateTokens(accessToken, refreshToken);
+    }
+
+    private boolean checkEmailWithPassword() {
+
+
+        return true;
+    }
 }
+
--- yata-server/src/main/java/project/yata/service/YataService.java
+++ yata-server/src/main/java/project/yata/service/YataService.java
@@ -1,10 +1,21 @@
 package project.yata.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import project.yata.entity.Account;
+import project.yata.persistence.AccountRepository;
 
 import java.util.List;
 
-public interface YataService {
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class YataService {
 
-    List<Account> getAccountList();
+    private final AccountRepository accountRepository;
+
+    public List<Account> getAccountList() {
+        return accountRepository.findAll();
+    }
 }
