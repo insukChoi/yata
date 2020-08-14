481f6ea63fdb46843b532609d6d731d9ee8ed86c
jisu <jisu@skoopmedia.co.kr>
2020-08-14 15:54:47 +0900

    join Service 테스트코드 끝

--- yata-server/src/test/java/project/yata/service/impl/AuthServiceImplTest.java
+++ yata-server/src/test/java/project/yata/service/impl/AuthServiceImplTest.java
@@ -1,50 +1,43 @@
 package project.yata.service.impl;
 
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.Test;
+
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
-import org.springframework.test.context.ActiveProfiles;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import project.yata.dto.JoinRequest;
 import project.yata.dto.JoinResponse;
 import project.yata.entity.Account;
 import project.yata.persistence.AccountRepository;
 import project.yata.service.AuthService;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.BDDAssertions.then;
 
-@ExtendWith(MockitoExtension.class)
+//@RunWith(SpringRunner.class)
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AuthServiceImplTest {
 
-    @InjectMocks
-    private AuthService authService;
+    @Autowired
+    AuthService authService;
 
-    @Mock
-    private Account account;
-
-    @Mock
-    private AccountRepository repository;
-
-    @BeforeAll
-    public void setUp() {
-        account = Account.builder().email("admin@yata.com").name("queen").password("pulledmytriggernowhesdead").build();
-    }
+    @Autowired
+    AccountRepository repository;
 
     @DisplayName("회원가입 성공")
     @Test
     void successJoin() {
         // given
-        final String email = account.getEmail();
-        final String name = account.getName();
-        final String password = account.getPassword();
+        final String email = "admin@yata.com";
+        final String name = "queen";
+        final String password = "pulledmytriggernowhesdead";
 
         JoinRequest joinRequest = JoinRequest.builder().email(email).name(name).password(password).build();
 
