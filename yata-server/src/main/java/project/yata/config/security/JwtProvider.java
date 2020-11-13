eadfc5548157843f149ae374881de58c4a8edc6f
jisu <jisu@skoopmedia.co.kr>
2020-11-13 11:40:03 +0900

    시큐리티 + jwt 작업 98% 완료

--- yata-server/src/main/java/project/yata/config/security/JwtProvider.java
+++ yata-server/src/main/java/project/yata/config/security/JwtProvider.java
@@ -4,14 +4,14 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
-@Service
+@Component
 public class JwtProvider {
 
     private String SECRET_KEY = "secret";
